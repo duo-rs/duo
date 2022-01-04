4b0237a439770b2de3e03a51988271040d2bd281
Folyd <lyshuhow@gmail.com>
2022-01-04 11:47:57 +0800

    Build jig protos

--- jig-api/build.rs
+++ jig-api/build.rs
@@ -1,4 +1,17 @@
+use std::error::Error;
 
-fn main() {
-    
+fn main() -> Result<(), Box<dyn Error>> {
+    let proto_files = &["proto/instrument.proto", "proto/span.proto"];
+
+    let dirs = &["proto"];
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .compile(proto_files, dirs)?;
+
+    // recompile protobufs only if any of the proto files changes.
+    for file in proto_files {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+    Ok(())
 }
---
+++ jig-api/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod instrument;
+
+pub mod span;
