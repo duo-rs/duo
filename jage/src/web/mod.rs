bcaec5bb75b46da69572cd7c2571bfd806e326fb
Folyd <lyshuhow@gmail.com>
2022-02-23 11:26:34 +0800

    Add fallback to avoid frontend routes 404

--- jage/src/web/mod.rs
+++ jage/src/web/mod.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
 use axum::{
-    http::StatusCode,
-    response::Html,
+    handler::Handler,
+    http::{StatusCode, Uri},
+    response::{Html, IntoResponse},
     routing::{get, get_service},
     AddExtensionLayer, Router,
 };
@@ -17,15 +18,15 @@ mod query;
 mod routes;
 mod serialize;
 
+// Frontend HTML page.
+static ROOT_PAGE: Html<&'static str> = Html(include_str!("../../ui/index.html"));
+
 pub struct JaegerData<I: IntoIterator>(pub I);
 
 pub async fn run_web_server(warehouse: Arc<RwLock<Warehouse>>) -> anyhow::Result<()> {
     let layer = ServiceBuilder::new().layer(AddExtensionLayer::new(warehouse));
     let app = Router::new()
-        .route(
-            "/",
-            get(|| async { Html(include_str!("../../ui/index.html")) }),
-        )
+        .route("/", get(|| async { ROOT_PAGE }))
         .nest(
             "/static",
             get_service(ServeDir::new("ui/static")).handle_error(|error| async move {
@@ -38,10 +39,24 @@ pub async fn run_web_server(warehouse: Arc<RwLock<Warehouse>>) -> anyhow::Result
         .route("/api/traces", get(routes::traces))
         .route("/api/services", get(routes::services))
         .route("/api/services/:service/operations", get(routes::operations))
+        .fallback(fallback.into_service())
         .layer(layer);
 
     axum::Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(app.into_make_service())
         .await?;
     Ok(())
+}
+
+async fn fallback(uri: Uri) -> impl IntoResponse {
+    let path = uri.path();
+    if path.starts_with("/api") || path.starts_with("/static") {
+        // For those routes, we simply return 404 text.
+        (StatusCode::NOT_FOUND, "404 Not Found").into_response()
+    } else {
+        // Due to the frontend is a SPA (Single Page Application),
+        // it has own frontend routes, we should return the ROOT PAGE
+        // to avoid frontend route 404.
+        (StatusCode::TEMPORARY_REDIRECT, ROOT_PAGE).into_response()
+    }
 }
