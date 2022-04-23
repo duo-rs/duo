98f21d6902cd031d137486283402564a96b3da42
Folyd <lyshuhow@gmail.com>
2022-04-23 12:19:25 +0800

    Update ascii art

--- duet/src/main.rs
+++ duet/src/main.rs
@@ -6,16 +6,15 @@ use parking_lot::RwLock;
 use tracing::Level;
 use tracing_subscriber::{filter::Targets, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
-// ASCII Art generated from https://patorjk.com/software/taag/#p=display&h=0&v=0&f=ANSI%20Shadow&t=Duet
+// ASCII Art generated from https://patorjk.com/software/taag/#p=display&h=0&v=0&f=ANSI%20Regular&t=Duet
 const JAGE_BANNER: &str = r"
-
-     ██╗ █████╗  ██████╗ ███████╗
-     ██║██╔══██╗██╔════╝ ██╔════╝
-     ██║███████║██║  ███╗█████╗  
-██   ██║██╔══██║██║   ██║██╔══╝  
-╚█████╔╝██║  ██║╚██████╔╝███████╗
- ╚════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
-                                 
+                                  
+██████  ██    ██ ███████ ████████ 
+██   ██ ██    ██ ██         ██    
+██   ██ ██    ██ █████      ██    
+██   ██ ██    ██ ██         ██    
+██████   ██████  ███████    ██    
+                                  
 ";
 
 #[tokio::main]
