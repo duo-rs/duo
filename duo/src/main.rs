209affdb7d270bac06901bd39bb6b3aed12966cd
Folyd <lyshuhow@gmail.com>
2023-05-20 12:36:29 +0800

    Remove lib.rs

--- duo/src/main.rs
+++ duo/src/main.rs
@@ -2,10 +2,21 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use clap::StructOpt;
-use duo::Warehouse;
 use parking_lot::RwLock;
 use tracing::Level;
 use tracing_subscriber::{filter::Targets, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+mod aggregator;
+mod grpc;
+mod models;
+mod warehouse;
+mod web;
+
+pub use aggregator::Aggregator;
+pub use grpc::spawn_server as spawn_grpc_server;
+pub use models::{Log, Process, Span, Trace, TraceExt};
+pub use warehouse::Warehouse;
+pub use web::run_web_server;
 
 // ASCII Art generated from https://patorjk.com/software/taag/#p=display&h=0&v=0&f=ANSI%20Regular&t=Duo
 static DUO_BANNER: &str = r"
@@ -53,8 +64,8 @@ async fn main() -> Result<()> {
             web_port,
             grpc_port,
         } => {
-            duo::spawn_grpc_server(Arc::clone(&warehouse), grpc_port);
-            duo::run_web_server(warehouse, web_port).await?;
+            spawn_grpc_server(Arc::clone(&warehouse), grpc_port);
+            run_web_server(warehouse, web_port).await?;
         }
     }
 
