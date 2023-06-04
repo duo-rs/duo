1431441cdb6b74cde4d6b711154eae0f88ff8faa
Folyd <lyshuhow@gmail.com>
2023-06-04 15:35:35 +0800

    Extract schemas

--- duo/src/arrow.rs
+++ duo/src/arrow.rs
@@ -1,9 +1,39 @@
 use std::{num::NonZeroU64, sync::Arc};
 
 use anyhow::Result;
-use arrow_array::{ArrayRef, Int64Array, RecordBatch, StringArray, UInt64Array, UInt8Array};
+use arrow_array::{Int64Array, RecordBatch, StringArray, UInt64Array, UInt8Array};
+use arrow_schema::{DataType, Field, Schema, SchemaRef};
 
 use crate::{Log, Span, Trace};
+
+pub fn schema_trace() -> SchemaRef {
+    Arc::new(Schema::new(vec![
+        Field::new("id", DataType::UInt64, false),
+        Field::new("duration", DataType::Int64, false),
+        Field::new("time", DataType::Int64, false),
+        Field::new("process_id", DataType::Utf8, false),
+    ]))
+}
+
+pub fn schema_span() -> SchemaRef {
+    Arc::new(Schema::new(vec![
+        Field::new("id", DataType::UInt64, false),
+        Field::new("parent_id", DataType::UInt64, true),
+        Field::new("trace_id", DataType::UInt64, false),
+        Field::new("name", DataType::Utf8, false),
+        Field::new("start", DataType::Int64, false),
+        Field::new("end", DataType::Int64, true),
+    ]))
+}
+
+pub fn schema_log() -> SchemaRef {
+    Arc::new(Schema::new(vec![
+        Field::new("span_id", DataType::UInt64, true),
+        Field::new("trace_id", DataType::UInt64, true),
+        Field::new("level", DataType::UInt8, false),
+        Field::new("time", DataType::Int64, false),
+    ]))
+}
 
 #[derive(Default)]
 pub struct TraceRecordBatchBuilder {
@@ -41,28 +71,15 @@ impl TraceRecordBatchBuilder {
     }
 
     pub fn into_record_batch(self) -> Result<RecordBatch> {
-        Ok(RecordBatch::try_from_iter_with_nullable(vec![
-            (
-                "id",
-                Arc::new(UInt64Array::from(self.trace_ids)) as ArrayRef,
-                false,
-            ),
-            (
-                "duration",
-                Arc::new(Int64Array::from(self.durations)) as ArrayRef,
-                false,
-            ),
-            (
-                "time",
-                Arc::new(Int64Array::from(self.trace_times)) as ArrayRef,
-                false,
-            ),
-            (
-                "process_id",
-                Arc::new(StringArray::from(self.process_ids)) as ArrayRef,
-                false,
-            ),
-        ])?)
+        Ok(RecordBatch::try_new(
+            schema_trace(),
+            vec![
+                Arc::new(UInt64Array::from(self.trace_ids)),
+                Arc::new(Int64Array::from(self.durations)),
+                Arc::new(Int64Array::from(self.trace_times)),
+                Arc::new(StringArray::from(self.process_ids)),
+            ],
+        )?)
     }
 }
 
@@ -77,38 +94,17 @@ impl SpanRecordBatchBuilder {
     }
 
     pub fn into_record_batch(self) -> Result<RecordBatch> {
-        Ok(RecordBatch::try_from_iter_with_nullable(vec![
-            (
-                "id",
-                Arc::new(UInt64Array::from(self.span_ids)) as ArrayRef,
-                false,
-            ),
-            (
-                "parent_id",
-                Arc::new(UInt64Array::from(self.parent_ids)) as ArrayRef,
-                true,
-            ),
-            (
-                "trace_id",
-                Arc::new(UInt64Array::from(self.trace_ids)) as ArrayRef,
-                false,
-            ),
-            (
-                "name",
-                Arc::new(StringArray::from(self.names)) as ArrayRef,
-                false,
-            ),
-            (
-                "start",
-                Arc::new(Int64Array::from(self.start_times)) as ArrayRef,
-                false,
-            ),
-            (
-                "end",
-                Arc::new(Int64Array::from(self.end_times)) as ArrayRef,
-                true,
-            ),
-        ])?)
+        Ok(RecordBatch::try_new(
+            schema_span(),
+            vec![
+                Arc::new(UInt64Array::from(self.span_ids)),
+                Arc::new(UInt64Array::from(self.parent_ids)),
+                Arc::new(UInt64Array::from(self.trace_ids)),
+                Arc::new(StringArray::from(self.names)),
+                Arc::new(Int64Array::from(self.start_times)),
+                Arc::new(Int64Array::from(self.end_times)),
+            ],
+        )?)
     }
 }
 
@@ -121,27 +117,14 @@ impl LogRecordBatchBuilder {
     }
 
     pub fn into_record_batch(self) -> Result<RecordBatch> {
-        Ok(RecordBatch::try_from_iter_with_nullable(vec![
-            (
-                "span_id",
-                Arc::new(UInt64Array::from(self.span_ids)) as ArrayRef,
-                true,
-            ),
-            (
-                "trace_id",
-                Arc::new(UInt64Array::from(self.trace_ids)) as ArrayRef,
-                true,
-            ),
-            (
-                "level",
-                Arc::new(UInt8Array::from(self.levels)) as ArrayRef,
-                false,
-            ),
-            (
-                "time",
-                Arc::new(Int64Array::from(self.times)) as ArrayRef,
-                false,
-            ),
-        ])?)
+        Ok(RecordBatch::try_new(
+            schema_log(),
+            vec![
+                Arc::new(UInt64Array::from(self.span_ids)),
+                Arc::new(UInt64Array::from(self.trace_ids)),
+                Arc::new(UInt8Array::from(self.levels)),
+                Arc::new(Int64Array::from(self.times)),
+            ],
+        )?)
     }
 }
