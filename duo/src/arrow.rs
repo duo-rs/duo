dde470019ef999d0594230347c49b5522dcdfa54
Folyd <lyshuhow@gmail.com>
2023-06-07 23:47:42 +0800

    Add `process_id` and `tags` for `Span` schema

--- duo/src/arrow.rs
+++ duo/src/arrow.rs
@@ -1,10 +1,26 @@
-use std::{num::NonZeroU64, sync::Arc};
+use datafusion::arrow::array::ArrayDataBuilder;
+use duo_api as proto;
+use std::{collections::HashMap, num::NonZeroU64, sync::Arc};
 
 use anyhow::Result;
-use arrow_array::{Int64Array, RecordBatch, StringArray, UInt64Array, UInt8Array};
-use arrow_schema::{DataType, Field, Schema, SchemaRef};
+use arrow_array::{Int64Array, MapArray, RecordBatch, StringArray, UInt64Array, UInt8Array};
+use arrow_schema::{DataType, Field, Fields, Schema, SchemaRef};
 
 use crate::{Log, Span};
+
+fn datatype_map() -> DataType {
+    DataType::Map(
+        Arc::new(Field::new(
+            "entries",
+            DataType::Struct(Fields::from(vec![
+                Field::new("keys", DataType::Utf8, false),
+                Field::new("values", DataType::UInt32, false),
+            ])),
+            false,
+        )),
+        false,
+    )
+}
 
 pub fn schema_span() -> SchemaRef {
     Arc::new(Schema::new(vec![
@@ -12,8 +28,10 @@ pub fn schema_span() -> SchemaRef {
         Field::new("parent_id", DataType::UInt64, true),
         Field::new("trace_id", DataType::UInt64, false),
         Field::new("name", DataType::Utf8, false),
+        Field::new("process_id", DataType::Utf8, false),
         Field::new("start", DataType::Int64, false),
         Field::new("end", DataType::Int64, true),
+        Field::new("tags", datatype_map(), true),
     ]))
 }
 
@@ -32,8 +50,10 @@ pub struct SpanRecordBatchBuilder {
     parent_ids: Vec<Option<u64>>,
     trace_ids: Vec<u64>,
     names: Vec<String>,
+    process_ids: Vec<String>,
     start_times: Vec<i64>,
     end_times: Vec<Option<i64>>,
+    tags: Vec<HashMap<String, proto::Value>>,
 }
 
 #[derive(Default)]
@@ -50,13 +70,17 @@ impl SpanRecordBatchBuilder {
         self.parent_ids.push(span.parent_id.map(|id| id.get()));
         self.trace_ids.push(span.trace_id.get());
         self.names.push(span.name.clone());
+        self.process_ids.push(span.process_id.clone());
         self.start_times
             .push((span.start.unix_timestamp_nanos() / 1000) as i64);
         self.end_times
             .push(span.end.map(|t| (t.unix_timestamp_nanos() / 1000) as i64));
+        self.tags.push(span.tags.clone());
     }
 
     pub fn into_record_batch(self) -> Result<RecordBatch> {
+        let map_data = ArrayDataBuilder::new(datatype_map()).build()?;
+        // TODO tags map
         Ok(RecordBatch::try_new(
             schema_span(),
             vec![
@@ -64,8 +88,10 @@ impl SpanRecordBatchBuilder {
                 Arc::new(UInt64Array::from(self.parent_ids)),
                 Arc::new(UInt64Array::from(self.trace_ids)),
                 Arc::new(StringArray::from(self.names)),
+                Arc::new(StringArray::from(self.process_ids)),
                 Arc::new(Int64Array::from(self.start_times)),
                 Arc::new(Int64Array::from(self.end_times)),
+                Arc::new(MapArray::from(map_data)),
             ],
         )?)
     }
