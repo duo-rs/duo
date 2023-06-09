b73e48293c30ca592dda1bfbb8c66b3f82836e47
Folyd <lyshuhow@gmail.com>
2023-06-09 22:54:19 +0800

    Remove message channel

--- duo/src/grpc/server.rs
+++ duo/src/grpc/server.rs
@@ -6,7 +6,6 @@ use proto::instrument::{
     instrument_server::Instrument, RecordEventRequest, RecordEventResponse, RecordSpanRequest,
     RecordSpanResponse, RegisterProcessRequest, RegisterProcessResponse,
 };
-use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tonic::{Request, Response, Status};
 use tracing::{debug, info};
 
@@ -15,57 +14,17 @@ use crate::{Aggregator, Warehouse};
 pub struct DuoServer {
     warehouse: Arc<RwLock<Warehouse>>,
     aggregator: Arc<RwLock<Aggregator>>,
-    sender: Sender<Message>,
-    receiver: Arc<RwLock<Receiver<Message>>>,
-}
-
-#[derive(Debug)]
-enum Message {
-    Register(RegisterMessage),
-    Span(proto::Span),
-    Log(proto::Log),
-}
-
-#[derive(Debug)]
-struct RegisterMessage {
-    tx: Sender<String>,
-    process: proto::Process,
 }
 
 impl DuoServer {
     pub fn new(warehouse: Arc<RwLock<Warehouse>>) -> Self {
-        let (sender, receiver) = channel::<Message>(4096);
         Self {
             warehouse,
             aggregator: Arc::new(RwLock::new(Aggregator::new())),
-            sender,
-            receiver: Arc::new(RwLock::new(receiver)),
         }
     }
 
     pub fn bootstrap(&mut self) {
-        let warehouse = Arc::clone(&self.warehouse);
-        let receiver = Arc::clone(&self.receiver);
-        let aggregator = Arc::clone(&self.aggregator);
-        tokio::spawn(async move {
-            loop {
-                let mut receiver = receiver.write();
-                match receiver.recv().await {
-                    Some(Message::Register(RegisterMessage { tx, process })) => {
-                        let process_id = warehouse.write().register_process(process);
-                        tx.send(process_id).await.unwrap();
-                    }
-                    Some(Message::Span(span)) => {
-                        aggregator.write().record_span(span);
-                    }
-                    Some(Message::Log(log)) => {
-                        aggregator.write().record_log(log);
-                    }
-                    None => {}
-                }
-            }
-        });
-
         let aggregator = Arc::clone(&self.aggregator);
         let warehouse = Arc::clone(&self.warehouse);
         tokio::spawn(async move {
@@ -105,17 +64,7 @@ impl Instrument for DuoServer {
             .into_inner()
             .process
             .ok_or_else(|| tonic::Status::invalid_argument("missing process"))?;
-
-        let (tx, mut rx) = channel(1024 * 100);
-        self.sender
-            .send(Message::Register(RegisterMessage { tx, process }))
-            .await
-            .map_err(|e| tonic::Status::internal(format!("register failed: {}", e)))?;
-
-        let process_id = rx
-            .recv()
-            .await
-            .ok_or_else(|| tonic::Status::internal("process id generated failed"))?;
+        let process_id = self.warehouse.write().register_process(process);
         Ok(Response::new(RegisterProcessResponse { process_id }))
     }
 
@@ -128,10 +77,7 @@ impl Instrument for DuoServer {
             .into_inner()
             .span
             .ok_or_else(|| tonic::Status::invalid_argument("missing span"))?;
-        self.sender
-            .send(Message::Span(span))
-            .await
-            .map_err(|e| tonic::Status::internal(format!("record span failed: {}", e)))?;
+        self.aggregator.write().record_span(span);
         Ok(Response::new(RecordSpanResponse {}))
     }
 
@@ -145,10 +91,7 @@ impl Instrument for DuoServer {
             .into_inner()
             .log
             .ok_or_else(|| tonic::Status::invalid_argument("missing event"))?;
-        self.sender
-            .send(Message::Log(log))
-            .await
-            .map_err(|e| tonic::Status::internal(format!("record event failed: {}", e)))?;
+        self.aggregator.write().record_log(log);
         Ok(Response::new(RecordEventResponse {}))
     }
 }
