e1f0865616d00e24a6ee0b8bfc82cc0c53f5f22b
Folyd <lyshuhow@gmail.com>
2024-08-14 17:17:42 +0800

    Add input and datepicker

--- duo-ui/src/routes/+layout.svelte
+++ duo-ui/src/routes/+layout.svelte
@@ -14,9 +14,9 @@
 
 <div class="m-5 flex flex-row items-center">
 	<h1 class="mx-5 text-2xl font-bold">DUO</h1>
-	<ul class="flex list-none flex-row text-xl">
+	<ul class="flex list-none flex-row text-md">
 		{#each menus as menu}
-			<li class="hover:bg-secondary rounded-md px-6 py-1">
+			<li class="hover:bg-secondary rounded-md px-5 py-1">
 				<a href={menu.path}>{menu.name}</a>
 			</li>
 		{/each}
--- duo-ui/src/routes/+page.svelte
+++ duo-ui/src/routes/+page.svelte
@@ -1,3 +1,89 @@
-<h1>Duo</h1>
+<script>
+	import CalendarIcon from 'lucide-svelte/icons/calendar';
+	import { Input } from '$lib/components/ui/input';
+	import { Button } from '$lib/components/ui/button';
+	import { onMount } from 'svelte';
+	import { DatePicker } from '@svelte-plugins/datepicker';
+	import { cn } from '$lib/utils';
+	import dayjs from 'dayjs';
 
-<p>Observability duet: Logging and Tracing</p>
+	/**
+	 * @type {string}
+	 */
+	let currentSevice;
+	/**
+	 * @type {string}
+	 */
+	let keyword;
+	/**
+	 * @type {number}
+	 */
+	let startDate;
+	/**
+	 * @type {number}
+	 */
+	let endDate;
+	/**
+	 * @type {string}
+	 */
+	let startDateTime;
+	/**
+	 * @type {string}
+	 */
+	let endDateTime;
+	/**
+	 * @type {number}
+	 */
+	let limit = 20;
+
+	let isOpen = false;
+	/**
+	 * @type {string}
+	 */
+	let pickedDateTimeRange = '';
+
+	$: {
+		if (startDate && endDate) {
+			pickedDateTimeRange = `${dayjs(startDate).format('MMM DD,YYYY')} ${startDateTime} - ${dayjs(endDate).format('MMM DD,YYYY')} ${endDateTime}`;
+		}
+	}
+	const toggleDatePicker = () => (isOpen = !isOpen);
+
+	function onSearch() {}
+
+	onMount(async () => {});
+</script>
+
+<div class="m-6">
+	<div class="flex items-center">
+		<Input class="mx-4 max-w-screen-md" placeholder="Search log by keyword" />
+		<Button on:click={onSearch}>Search</Button>
+		<div class="mx-6">
+			<DatePicker
+				bind:isOpen
+				bind:startDate
+				bind:endDate
+				bind:startDateTime
+				bind:endDateTime
+				align={'right'}
+				isRange
+				isMultipane
+				showTimePicker
+				showPresets
+				enableFutureDates={false}
+			>
+				<Button
+					variant="outline"
+					class={cn(
+						'w-[300px] justify-start text-left font-normal',
+						!startDate && 'text-muted-foreground'
+					)}
+					on:click={toggleDatePicker}
+				>
+					<CalendarIcon class="mr-2 h-4 w-4" />
+					{pickedDateTimeRange ? pickedDateTimeRange : 'Pick a date'}
+				</Button>
+			</DatePicker>
+		</div>
+	</div>
+</div>
